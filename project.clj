3af3177c36d1f2f32a27726f244d4fa4f39693ae
Korny Sietsma <korny@sietsma.com>
2018-03-27 12:19:21 +0100

    updated libraries, project info, bumped to version 1.0.0

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject indent2flare "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.cli "0.3.3"]
-                 [cheshire "5.5.0"]
+(defproject indent2flare "1.0.0"
+  :description "Simple tool to add file indentation statistics to a d3 json file"
+  :url "https://github.com/kornysietsma/indent2flare"
+  :license {:name "Apache 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [cheshire "5.8.0"]
                  [com.stuartsierra/frequencies "0.1.0"]]
   :main indent2flare.cli
   :uberjar-name "indent2flare.jar"
   :profiles {
              :uberjar {:aot :all}
-             :dev     {:dependencies [[midje "1.8.3"]]}
+             :dev     {:dependencies [[midje "1.9.1"]]}
              }
   :plugins [[lein-midje "3.2"]]
   )
